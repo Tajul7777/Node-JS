be8ae45b1f312d32a217cad31e83667a37635ae0
Tajul7777 <mdtajulislam7777@gmail.com>
2020-02-24 00:04:03 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log("Tajul");
+//###01 setTimeout
+// setTimeout(function(){
+//   console.log("Tajul 3 sec passed");
+// }, 3000);
+
+//###02 setInterval
+// var time = 0;
+// setInterval (function(){
+//   time += 1;
+//   console.log(time +"second passed");
+// }, 1000)
+
+//###setInterval with condition
+var time = 0;
+var timer = setInterval(function(){
+  time +=2;
+  if(time>5)
+  {
+    clearInterval(timer);
+  }
+  else {
+    console.log(time +"sec passed");
+  }
+},2000);
